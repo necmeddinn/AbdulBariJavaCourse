17bd5e9c286c73d9c7ad9aa5d51e72f275c5471d
necmeddinn <ncnd87@gmail.com>
2025-02-17 01:32:18 +0300

    Commit 1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println(args[0]);
+        //System.out.println(args[0]);
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the name : ");
+        if (sc.hasNextInt()){
+            int num = sc.nextInt();
+            System.out.println("The input number is : " + num);
+        } else {
+            System.out.println("Invalid input!!!");
+        }
+
     }
 }
