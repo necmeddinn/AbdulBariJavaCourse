b9afbffdf8d60e10761c0220b61e6a058f505ad6
necmeddinn <ncnd87@gmail.com>
2025-02-24 15:31:42 +0300

    String Practice

---
+++ StringClass/src/StringClass.java
@@ -0,0 +1,23 @@
+public class StringClass{
+    public static void main(String[] args) {
+//        String str1 = "Java program";
+//
+//        String str2 = new String("JAVA");
+//
+//        char c[] = {'H','e','l','l','o'};
+//        String str3 = new String(c,1,3);
+//
+//        byte b[] = {65,66,67,68};
+//        String str4 = new String(b,1,3);
+//
+//        System.out.println(str3);
+//
+//        System.out.println(str4);
+
+        String s1 = "Java";
+        String s2 = new String("Java");
+        System.out.println(s1 == s2);
+
+
+    }
+}
