2c420754c2ded1f8fa5399975a244b8eea51c07c
necmeddinn <ncnd87@gmail.com>
2025-03-02 01:14:18 +0300

    Examples for Methods

--- Methods/src/ParameterPassing.java
+++ Methods/src/ParameterPassing.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class ParameterPassing {
+
+    static boolean isPrime(int n) {
+        for (int i = 2; i < n/2; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // Not : Gerçek parametrelerin içeriği resmi parametrelerde kopyalanır.
+
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+
+        System.out.println(isPrime(num) ? "Prime" : "Not prime");
 
     }
 }
